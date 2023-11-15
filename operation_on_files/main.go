40d2d6fadc39962719205439517a9ada728d3759
Olartbaraq <akanbi.ges201@gmail.com>
2023-11-15 19:25:27 +0100

    files done

--- operation_on_files/main.go
+++ operation_on_files/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"os"
@@ -86,28 +87,101 @@ func main() {
 	//
 
 	//WRITING A BYTE SLICE TO A FILE
-	file, err := os.OpenFile("new.txt", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
-	if err != nil {
-		log.Fatal(err)
-	}
+	// file, err := os.OpenFile("new.txt", os.O_CREATE|os.O_TRUNC|os.O_WRONLY, 0644)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
 	// fileInfo, err := os.Stat("new.txt")
 	// p(fileInfo.Name(), fileInfo.Mode(), fileInfo.Size())
 
 	// To write a byte slice to a file, we first convert the string to a byte slice then write
 	// each rune/byte to the file; this can also give room to modify each byte since its mutable
 
-	byteSlice := []byte("I am enjoyin Golang")
-	bytesWritten, err := file.Write(byteSlice)
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Printf("No of bytes written is %d", bytesWritten)
+	// byteSlice := []byte("I am enjoyin Golang")
+	// bytesWritten, err := file.Write(byteSlice)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("No of bytes written is %d", bytesWritten)
 
 	// Another way is to use the os.WriteFile function.
 
-	bc := []byte("Still enjoying Golang")
-	err = os.WriteFile("new.txt", bc, 0644)
-	if err != nil {
-		log.Fatal(err)
+	// bc := []byte("Still enjoying Golang")
+	//err = os.WriteFile("new.txt", bc, 0644)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+
+	//
+	//
+	//
+
+	//READING A FILE
+
+	// file, err := os.Open("new.txt")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer file.Close()
+
+	// byteSlice := make([]byte, 3)
+	// numberOfBytesRead, err := io.ReadFull(file, byteSlice)
+
+	// fmt.Printf("Number of bytes Read is %v\n", numberOfBytesRead)
+	// fmt.Printf("Data read is %q\n", byteSlice) // This retuurned the number of bytes read which was 3.
+
+	// To read the actual byte without specifying the number of bytes presumably; we use io.ReadAll.
+
+	// file, err = os.Open("new.txt")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer file.Close()
+
+	// dataReader, err := io.ReadAll(file)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("Data read is \n %s\n", (dataReader)) // This will print out the entire data read from the file new.txt.
+
+	//
+	//Another way is to use os.ReadFile() to read the entire file.
+	// file, err := os.Open("new.txt")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// defer file.Close()
+
+	// dataReader, err := os.ReadFile("new.txt")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// fmt.Printf("Data read is \n %s\n", (dataReader)) // This will print out the entire data read from the file new.txt.
+
+	//
+	//
+	//
+
+	//SCANNING FOR USER INPUT, READING FROM STDIN
+	scanner := bufio.NewScanner(os.Stdin)
+
+	scanner.Scan()
+
+	text := scanner.Text()
+	//bytes := scanner.Bytes()
+	//fmt.Printf("Text entered is %s\n", text)
+
+	// To continuously scan the stdin until a particualr string is scaned, we scan using the for loop
+	for scanner.Scan() {
+		text = scanner.Text()
+		fmt.Printf("You entered %s\n", text)
+		if text == "exit" {
+			fmt.Printf("No more entry needed\n")
+			break
+		}
 	}
+	if err := scanner.Err(); err != nil {
+		log.Println(err)
+	}
+	fmt.Printf("Waiting for instructions...\n")
 }
